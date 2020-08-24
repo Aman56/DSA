b973dc8a5fe3916a8db3ba4f51b8a339b26a86f6
Aman <aman56.champ@gmail.com>
2020-08-24 23:12:33 +0530

    Create flow007.py

---
+++ flow007.py
@@ -0,0 +1,10 @@
+# cook your dish here
+t = int(input())
+for i in range(t):
+    n = int(input())
+    reverse = 0
+    while(n>0):
+        remainder = n%10
+        reverse = reverse*10 + remainder
+        n = n//10
+    print(reverse)
